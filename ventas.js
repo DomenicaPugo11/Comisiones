e70df5d55f8d0d14dfbb68f208a2a5eeda56c6ea
DomenicaPugo11 <evlyn562@gmail.com>
2026-09-23 17:48:29 -0500

    Calculo de comsion y resultados en pantalla

---
+++ ventas.js
@@ -0,0 +1,38 @@
+const VENTAS_BASE=5;
+
+function calcularComision(numeroVenta,precioProducto){
+    let comision=0;
+    if (numeroVenta>VENTAS_BASE){
+        let ventasExtras=numeroVenta-VENTAS_BASE;
+        comision= ventasExtras * (precioProducto*0.10);
+    }
+    return comision;
+}
+
+function calcular(){
+    //Recuperamos propiedades de la caja de texto
+    let ctxSueldoBase=document.getElementById("txtSueldoBase");
+    let ctxVentas=document.getElementById("txtVentas");
+    let ctxPrecio=document.getElementById("txtPrecio");
+
+    //Recuperamos el valor de las cajas de texto
+    let suelsoBaseStr=ctxSueldoBase.value;
+    let numeroVentasStr=ctxVentas.value;
+    let precioProductoStr=ctxPrecio.value;
+
+    //Convertimos el texto a numero
+    let sueldoBase=parseFloat(suelsoBaseStr);
+    let numeroVentas=parseFloat(numeroVentasStr);
+    let precioProducto=parseFloat(precioProductoStr);
+
+    let comision=calcularComision(numeroVentas,precioProducto);
+    let total=sueldoBase+comision;
+
+    let spSueldoBase=document.getElementById("spSueldoBase");
+    let spComision=document.getElementById("spComision");
+    let spTotal=document.getElementById("spTotal");
+
+    spSueldoBase.textContent=sueldoBase;
+    spComision.textContent=comision;
+    spTotal.textContent=total;
+}
